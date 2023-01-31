0c1460b2c47d5064ad6e7360304657036cd18366
Vincenz Herzog <herzogv88@gmail.com>
2023-01-31 19:59:39 +0100

    Setup MongoDB model for stories

--- src/mongodb/models/story.js
+++ src/mongodb/models/story.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const storySchema = new mongoose.Schema({
   title: { type: String, required: true, min: 5 },
   description: { type: String, required: true, min: 10, max: 1024 },
-  date: {
+  created: {
+    type: Date,
+    trim: true,
+    default: Date.now,
+  },
+  updated: {
     type: Date,
     trim: true,
     default: Date.now,
