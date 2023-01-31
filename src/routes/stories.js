382b638e9110018d2fe8837822f1590fc675b007
Vincenz Herzog <herzogv88@gmail.com>
2023-01-31 19:58:44 +0100

    Setup routes for stories

--- src/routes/stories.js
+++ src/routes/stories.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
-const { validateStory } = require("../validation/validation");
+const { validateStory, validateUpdatedStory } = require("../validation/validation");
 const authorization = require("../middleware/authorization");
-const { createStory } = require("../helpers/storyHelper");
+const { createStory, updateStory, deleteStory } = require("../helpers/storyHelper");
 
 //Stories
 //GET /stories
@@ -10,7 +10,7 @@ router.get("/", authorization, (req, res) => {});
 
 //GET /stories/new
 router.get("/new", authorization, (req, res) => {
-  res.render("story", {
+  res.render("storyNew", {
     title: `Create a new Story`,
     layout: "./layouts/main",
     user: {
@@ -26,7 +26,7 @@ router.post("/new", authorization, async (req, res) => {
   const { error } = validateStory(req.body);
   if (error) {
     const { title, description } = req.body;
-    res.status(400).render("story", {
+    res.status(400).render("storyNew", {
       title: "Create a new Story",
       error: error.details[0].message,
       title,
@@ -35,7 +35,84 @@ router.post("/new", authorization, async (req, res) => {
     return;
   }
   //Create new Story
-  await createStory(req, res);
+  const result = await createStory(req, res);
+  if (result.status) {
+    //Flash Message - Story created
+    req.flash("success_msg", "Story successfuly created!");
+    //Redirect to Dashboard
+    res.status(200).redirect("/dashboard");
+    return;
+  }
+  res.status(500).send();
+  //Flash Message - DB Failure
+  req.flash("error_msg", `Database Error: ${result}`);
+  //Redirect to Login
+  res.redirect("/dashboard");
+});
+
+//Story Details / Edit Story
+router.get("/details", authorization, (req, res) => {
+  res.render("storyDetail", {
+    title: `Edit your Story`,
+    layout: "./layouts/main",
+    user: {
+      userID: req.user.userID,
+    },
+    story: {
+      id: req.query.story,
+      title: req.query.title,
+      description: req.query.desc,
+      lastmodified: req.query.lastmodified,
+    },
+  });
+});
+
+//Update Story
+//POST /stories
+router.post("/update", authorization, async (req, res) => {
+  console.log(req.body);
+  //Check for valid body (story) - Check HTML Form Data
+  const { error } = validateUpdatedStory(req.body);
+  if (error) {
+    const { storyID, title, description } = req.body;
+    res.status(400).render("storyDetail", {
+      error: error.details[0].message,
+      storyID,
+      title,
+      description,
+    });
+    return;
+  }
+  //Update Story
+  const result = await updateStory(req, res);
+  if (result.status) {
+    //Flash Message - Story updated
+    req.flash("success_msg", "Story successfuly updated!");
+    //Redirect to Dashboard
+    res.redirect("/dashboard");
+    return;
+  }
+  res.status(500).send();
+  //Flash Message - DB Failure
+  req.flash("error_msg", `Database Error: ${result}`);
+  //Redirect to Login
+  res.redirect("/dashboard");
+});
+
+//Delete Story
+//DELETE /stories/delete/:id
+router.get("/delete/:id", authorization, async (req, res) => {
+  //Delete Story
+  const result = await deleteStory(req, res);
+  if (result) {
+    //Flash Message - Story updated
+    req.flash("success_msg", "Story successfuly updated!");
+    res.status(200).send();
+    return;
+  }
+  res.status(500).send();
+  //Flash Message - DB Failure
+  req.flash("error_msg", `Database Error: ${result}`);
 });
 
 module.exports = router;
